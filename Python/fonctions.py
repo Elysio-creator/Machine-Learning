2eb67d57c60ebb89d4c9e1f3339959bd9dc1c4a0
Elysio-creator <samy.68100@gmail.com>
2025-05-14 19:52:06 +0200

    projet Machine Learning

---
+++ Python/fonctions.py
@@ -0,0 +1,36 @@
+import math
+
+liste = [1,3,8,5,"yte"]
+
+def nom_function():
+    a = math.cos(20)
+
+a = math.cos(20)
+
+    
+print("jkhdsvfhjvf",liste[-1], "shjdbjbsdhv")
+
+print(f"la valeur de a est = {a} jhggrbguybgr {liste[0]} neruerhgiu {liste[-1]}")
+    
+for samy in liste:
+    print(samy)
+
+for i in range(1,len(liste)):
+    print(liste[i])
+    
+   
+class Personne:
+    def __init__(self,nom,prenom):
+        self.__nom=nom
+        self.__prenom=prenom
+    
+class Directeur(Personne):
+    def __init__(self, nom, prenom,entreprise):
+        self.__entreprise=entreprise
+        super().__init__(nom,prenom)
+
+directeur1=Directeur("Terry","Rab",)        
+                
+
+    
+    
